3d41386a9982408bbd92ae400ec0236edb44712d
Jordan Kagmeni <kagmeni77@gmail.com>
2020-06-03 02:22:58 +0200

    frontend: create a custom element of app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
+/* eslint-disable class-methods-use-this */
 import 'babel-polyfill';
 import React from 'react';
-import { render } from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import ThunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducers from './reducers/rootReducer';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
 
+/**
+ * STORE
+ * @const store
+ * @description Creation store of application
+ */
 const store = createStore(rootReducers, applyMiddleware(ThunkMiddleware));
-render(
-  <App store={store}/>,
-  document.querySelector('#root')
-);
+
+/**
+ * E-VOTING APP ELEMENT
+ * @class EVotingApp
+ * @description Custom Component created and connected with React
+ */
+class EVotingApp extends HTMLElement {
+  connectedCallback() {
+    render(<App store={store}/>, this);
+  }
+
+  disconnectedCallback() {
+    unmountComponentAtNode(this);
+  }
+}
+
+customElements.define('evoting-app', EVotingApp);
 
 // The app work offline and load faster. You can change this to unregister() if you don't want it.
 serviceWorker.register();
