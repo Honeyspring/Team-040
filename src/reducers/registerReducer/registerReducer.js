885ceabf565365525058185fb195c93c43c81450
Jordan Kagmeni <kagmeni77@gmail.com>
2020-06-04 15:10:47 +0200

    register: define register reducer

---
+++ src/reducers/registerReducer/registerReducer.js
@@ -0,0 +1,43 @@
+import { initialStore } from '../store';
+import * as types from '../../constants/registerActionTypes';
+
+/**
+ * REGISTER REDUCER
+ * @func registerReducer
+ * @param {object} state
+ * @param {object} action
+ * @description Reducer for register user feature
+ * @returns object
+ */
+const registerReducer = (state = initialStore, action = {}) => {
+  switch (action.type) {
+    case types.SIGN_UP:
+      return {
+        ...state,
+        loading: action.loading
+      }
+    case types.SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        loading: action.loading,
+        success: {
+          ...state.success,
+          message: action.payload.message
+        }
+      }
+    case types.SIGN_UP_ERROR:
+      return {
+        ...state,
+        loading: action.loading,
+        error: {
+          ...state.error,
+          message: action.error
+        }
+      }
+  
+    default:
+      return state;
+  }
+};
+
+export default registerReducer;
