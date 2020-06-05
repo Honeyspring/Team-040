560a74916a926874968a9659d5191af8913663c9
Jordan Kagmeni <kagmeni77@gmail.com>
2020-06-05 14:43:25 +0200

    register: update file add registerService route and setup toast

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { toast } from 'react-toastify';
 import propTypes from 'prop-types';
-/* import Welcome from './Components/Welcome/Welcome';
-first welcome page,switch between  them with this */
+
 import Welcome from './Components/Welcome/Welcom';
-import SignIn from './Components/Welcome/SignIn';
-import Register from './Components/Welcome/Register';
+import SignIn from './Components/SignIn/SignIn';
+import RegisterService from './containers/registerService/registerService';
 
 /**
  * ROUTER APP COMPONENT
@@ -17,14 +15,15 @@ import Register from './Components/Welcome/Register';
  */
 
 const App = ({ store }) => {
-  toast.configure(); // Init the toast notification in Application globaly
+  toast.configure({position: 'top-right', hideProgressBar: true}); // Init the toast notification in Application globaly
   return (
     <Provider store={store}>
       <Router>
         <Switch>
+          <Route exact path="/" component={Welcome} />
           <Route path="/signIn" component={SignIn} />
-          <Route path="/Register" component={Register} />
-          <Route path="/Welcome" component={Welcome} />
+          <Route path="/register" component={RegisterService} />
+          <Route component={() => <h1>404 PAGE NOT FOUND</h1>} />
         </Switch>
       </Router>
     </Provider>
