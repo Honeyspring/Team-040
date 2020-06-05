72723ec822f7f270c1a2222b59bd78c7628c08ca
Jordan Kagmeni <kagmeni77@gmail.com>
2020-06-05 20:04:22 +0200

    login: define login service connect Sign In component to redux

---
+++ src/containers/loginService/loginService.js
@@ -0,0 +1,37 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import loginThunk from '../../middlewares/loginThunk';
+import SignIn from '../../Components/SignIn/SignIn';
+
+/**
+ * MAP DISPATCH TO PROPS
+ * @func mapDispatchToProps
+ * @param {function} dispatch
+ * @description Map dispatch to props set props on props component who is connected him and received actions should come from component to dispatch
+ * @returns object<Function>
+ */
+const mapDispatchToProps = (dispatch) => ({
+  loginUser: bindActionCreators(loginThunk, dispatch)
+});
+
+/**
+ * MAP STATE TO PROPS
+ * @func mapStateToProps
+ * @param {object} state
+ * @description Map state to props pass necessary props to component who is connected him
+ * @returns object
+ */
+const mapStateToProps = (state) => ({
+  loading: state.loginReducer.loading,
+  error: state.loginReducer.error
+});
+
+/**
+ * LOGIN SERVICE
+ * @const LoginService
+ * @description Connect component to redux and pass all props
+ */
+const LoginService = connect(mapStateToProps, mapDispatchToProps)(SignIn);
+
+export default LoginService;
