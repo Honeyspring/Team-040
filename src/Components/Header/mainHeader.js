7329674ec7f6fe619c2e0e82e78c5f6ba3fa5bbb
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-06-10 02:52:23 +0100

    add user getting started sections

--- src/Components/Header/mainHeader.js
+++ src/Components/Header/mainHeader.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
-import logo from '../assets/icons/logo.ico';
-import menu from '../assets/icons/menu.ico';
+import logo from '../../assets/icons/logo.ico';
+import menu from '../../assets/icons/menu.ico';
 
 
 const Header = () => (
