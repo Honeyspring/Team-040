120573e2b4c1d42ba52fb62781caa19de2d4a554
Torador <kagmeni77@gmail.com>
2020-06-13 14:23:22 +0200

    frontend: refactor footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,18 +6,16 @@ import github from '../../assets/icons/github.ico';
 
 const Footer = () => (
   <footer className="footer" id="footer">
-    <a className=" dim" target="blank" href="https://www.facebook.com/groups/249164319589395/">
-      <img className="icons" src={facebook} alt="facebook" />
+    <a className=" dim" href="https://www.facebook.com/groups/249164319589395/">
+      <img className="mw2" src={facebook} alt="facebook" />
     </a>
 
-            <a className=' dim' href="https://www.facebook.com/groups/249164319589395/"><img className="mw2" src={facebook} alt='facebook'/></a>
+    <a className=" dim" href="https://github.com/BuildForSDG/Team-040">
+      <img className="mw2" src={github} alt="github" />
+    </a>
 
-            <a className=' dim' href="https://github.com/BuildForSDG/Team-040"><img className="mw2" src={github} alt='github'/></a>
-
-           <p className=' link dim  pa3 dark-gray '>&copy;  {new Date().getFullYear()}   TEAM-040 #BuildForSDDG</p>
-
-
-        </footer>
+    <p className=" link dim  pa3 dark-gray ">&copy; {new Date().getFullYear()} TEAM-040 #BuildForSDDG</p>
+  </footer>
 );
 
 export default Footer;
